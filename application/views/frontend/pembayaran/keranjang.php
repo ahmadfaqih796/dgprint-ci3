9d61f3cebb41452ca7382b0cb6a1b9c9651483ef
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-07-28 20:54:06 +0700

    keranjang

--- application/views/frontend/pembayaran/keranjang.php
+++ application/views/frontend/pembayaran/keranjang.php
@@ -1,6 +1,6 @@
 <?php
-if ($keranjang['keranjang_total'] == '0'):
-	?>
+if ($keranjang['keranjang_total'] == 0 || $keranjang == null) :
+?>
 	<div class="container">
 		<div class="text-center"><i class="fa fa-cart-arrow-down empty-cart-icon"></i>
 			<p class="lead">Keranjang Kamu Kosong</p><a class="btn btn-primary btn-lg" href="<?= base_url() ?>">Pesan
@@ -8,8 +8,8 @@ if ($keranjang['keranjang_total'] == '0'):
 		</div>
 	</div>
 <?php
-elseif ($keranjang == !null):
-	?>
+elseif ($keranjang == !null) :
+?>
 	<div class="container">
 		<header class="page-header">
 			<h1 class="page-title">Keranjang</h1>
@@ -17,43 +17,39 @@ elseif ($keranjang == !null):
 		<div class="row">
 			<div class="col-md-10">
 				<?php
-				if ($stiker != null):
-					?>
+				if ($stiker != null) :
+				?>
 					<h4>Stiker</h4>
 					<table class="table table-bordered table-shopping-cart">
 						<thead>
-						<tr>
-							<th>Foto</th>
-							<th>Panjang</th>
-							<th>Lebar</th>
-							<th>Bahan</th>
-							<th>Jumlah</th>
-							<th>Estimasi</th>
-							<th>Total</th>
-							<th>Hapus</th>
-						</tr>
+							<tr>
+								<th>Foto</th>
+								<th>Panjang</th>
+								<th>Lebar</th>
+								<th>Bahan</th>
+								<th>Jumlah</th>
+								<th>Estimasi</th>
+								<th>Total</th>
+								<th>Hapus</th>
+							</tr>
 						</thead>
 						<tbody>
-						<?php
-						foreach ($stiker as $key => $value):
+							<?php
+							foreach ($stiker as $key => $value) :
 							?>
-							<tr>
-								<td><img src="<?= base_url('assets/images/stiker/') . $value['stiker_foto'] ?>"
-										 alt="foto"
-										 style="width: 100%"></td>
-								<td><?= $value['stiker_panjang'] ?> m</td>
-								<td><?= $value['stiker_lebar'] ?> m</td>
-								<td><?= $value['stiker_bahan'] ?></td>
-								<td><?= $value['stiker_jumlah'] ?> </td>
-								<td><?= $value['stiker_estimasi'] ?> Hari</td>
-								<td style="text-align: right"> Rp.<?= nominal($value['stiker_total']) ?></td>
-								<td><a class="fa fa-close table-shopping-remove"
-									   href="<?= base_url('hapus/stiker/' . $value['stiker_id']) ?>"
-									   onclick="return confirm('Hapus Pesanan? ')"></a></td>
-							</tr>
-						<?php
-						endforeach;
-						?>
+								<tr>
+									<td><img src="<?= base_url('assets/images/stiker/') . $value['stiker_foto'] ?>" alt="foto" style="width: 100%"></td>
+									<td><?= $value['stiker_panjang'] ?> m</td>
+									<td><?= $value['stiker_lebar'] ?> m</td>
+									<td><?= $value['stiker_bahan'] ?></td>
+									<td><?= $value['stiker_jumlah'] ?> </td>
+									<td><?= $value['stiker_estimasi'] ?> Hari</td>
+									<td style="text-align: right"> Rp.<?= nominal($value['stiker_total']) ?></td>
+									<td><a class="fa fa-close table-shopping-remove" href="<?= base_url('hapus/stiker/' . $value['stiker_id']) ?>" onclick="return confirm('Hapus Pesanan? ')"></a></td>
+								</tr>
+							<?php
+							endforeach;
+							?>
 						</tbody>
 					</table>
 					<div class="gap gap-small"></div>
@@ -61,43 +57,39 @@ elseif ($keranjang == !null):
 				endif;
 				?>
 				<?php
-				if ($spanduk != null):
-					?>
+				if ($spanduk != null) :
+				?>
 					<h4>Spanduk</h4>
 					<table class="table table-bordered table-shopping-cart">
 						<thead>
-						<tr>
-							<th>Foto</th>
-							<th>Panjang</th>
-							<th>Lebar</th>
-							<th>Bahan</th>
-							<th>Jumlah</th>
-							<th>Estimasi</th>
-							<th>Total</th>
-							<th>Hapus</th>
-						</tr>
+							<tr>
+								<th>Foto</th>
+								<th>Panjang</th>
+								<th>Lebar</th>
+								<th>Bahan</th>
+								<th>Jumlah</th>
+								<th>Estimasi</th>
+								<th>Total</th>
+								<th>Hapus</th>
+							</tr>
 						</thead>
 						<tbody>
-						<?php
-						foreach ($spanduk as $key => $value):
+							<?php
+							foreach ($spanduk as $key => $value) :
 							?>
-							<tr>
-								<td><img src="<?= base_url('assets/images/spanduk/') . $value['spanduk_foto'] ?>"
-										 alt="foto"
-										 style="width: 100%"></td>
-								<td><?= $value['spanduk_panjang'] ?> m</td>
-								<td><?= $value['spanduk_lebar'] ?> m</td>
-								<td><?= $value['spanduk_bahan'] ?></td>
-								<td><?= $value['spanduk_jumlah'] ?> </td>
-								<td><?= $value['spanduk_estimasi'] ?> Hari</td>
-								<td style="text-align: right"> Rp.<?= nominal($value['spanduk_total']) ?></td>
-								<td><a class="fa fa-close table-shopping-remove"
-									   href="<?= base_url('hapus/spanduk/' . $value['spanduk_id']) ?>"
-									   onclick="return confirm('Hapus Pesanan? ')"></a></td>
-							</tr>
-						<?php
-						endforeach;
-						?>
+								<tr>
+									<td><img src="<?= base_url('assets/images/spanduk/') . $value['spanduk_foto'] ?>" alt="foto" style="width: 100%"></td>
+									<td><?= $value['spanduk_panjang'] ?> m</td>
+									<td><?= $value['spanduk_lebar'] ?> m</td>
+									<td><?= $value['spanduk_bahan'] ?></td>
+									<td><?= $value['spanduk_jumlah'] ?> </td>
+									<td><?= $value['spanduk_estimasi'] ?> Hari</td>
+									<td style="text-align: right"> Rp.<?= nominal($value['spanduk_total']) ?></td>
+									<td><a class="fa fa-close table-shopping-remove" href="<?= base_url('hapus/spanduk/' . $value['spanduk_id']) ?>" onclick="return confirm('Hapus Pesanan? ')"></a></td>
+								</tr>
+							<?php
+							endforeach;
+							?>
 						</tbody>
 					</table>
 					<div class="gap gap-small"></div>
@@ -105,38 +97,35 @@ elseif ($keranjang == !null):
 				endif;
 				?>
 				<?php
-				if ($kartu != null):
-					?>
+				if ($kartu != null) :
+				?>
 					<h4>Kartu Nama</h4>
 					<table class="table table-bordered table-shopping-cart">
 						<thead>
-						<tr>
-							<th>Foto</th>
-							<th>Bahan</th>
-							<th>Jumlah</th>
-							<th>Estimasi</th>
-							<th>Total</th>
-							<th>Hapus</th>
-						</tr>
+							<tr>
+								<th>Foto</th>
+								<th>Bahan</th>
+								<th>Jumlah</th>
+								<th>Estimasi</th>
+								<th>Total</th>
+								<th>Hapus</th>
+							</tr>
 						</thead>
 						<tbody>
-						<?php
-						foreach ($kartu as $key => $value):
+							<?php
+							foreach ($kartu as $key => $value) :
 							?>
-							<tr>
-								<td><img src="<?= base_url('assets/images/kartu/') . $value['kartu_foto'] ?>" alt="foto"
-										 style="width: 100%"></td>
-								<td><?= $value['kartu_bahan'] ?></td>
-								<td><?= $value['kartu_jumlah'] ?> </td>
-								<td><?= $value['kartu_estimasi'] ?> Hari</td>
-								<td style="text-align: right"> Rp.<?= nominal($value['kartu_total']) ?></td>
-								<td><a class="fa fa-close table-shopping-remove"
-									   href="<?= base_url('hapus/kartu/' . $value['kartu_id']) ?>"
-									   onclick="return confirm('Hapus Pesanan? ')"></a></td>
-							</tr>
-						<?php
-						endforeach;
-						?>
+								<tr>
+									<td><img src="<?= base_url('assets/images/kartu/') . $value['kartu_foto'] ?>" alt="foto" style="width: 100%"></td>
+									<td><?= $value['kartu_bahan'] ?></td>
+									<td><?= $value['kartu_jumlah'] ?> </td>
+									<td><?= $value['kartu_estimasi'] ?> Hari</td>
+									<td style="text-align: right"> Rp.<?= nominal($value['kartu_total']) ?></td>
+									<td><a class="fa fa-close table-shopping-remove" href="<?= base_url('hapus/kartu/' . $value['kartu_id']) ?>" onclick="return confirm('Hapus Pesanan? ')"></a></td>
+								</tr>
+							<?php
+							endforeach;
+							?>
 						</tbody>
 					</table>
 					<div class="gap gap-small"></div>
@@ -144,39 +133,35 @@ elseif ($keranjang == !null):
 				endif;
 				?>
 				<?php
-				if ($brosur != null):
-					?>
+				if ($brosur != null) :
+				?>
 					<h4>Brosur</h4>
 					<table class="table table-bordered table-shopping-cart">
 						<thead>
-						<tr>
-							<th>Foto</th>
-							<th>Bahan</th>
-							<th>Jumlah</th>
-							<th>Estimasi</th>
-							<th>Total</th>
-							<th>Hapus</th>
-						</tr>
+							<tr>
+								<th>Foto</th>
+								<th>Bahan</th>
+								<th>Jumlah</th>
+								<th>Estimasi</th>
+								<th>Total</th>
+								<th>Hapus</th>
+							</tr>
 						</thead>
 						<tbody>
-						<?php
-						foreach ($brosur as $key => $value):
+							<?php
+							foreach ($brosur as $key => $value) :
 							?>
-							<tr>
-								<td><img src="<?= base_url('assets/images/brosur/') . $value['brosur_foto'] ?>"
-										 alt="foto"
-										 style="width: 100%"></td>
-								<td><?= $value['brosur_bahan'] ?></td>
-								<td><?= $value['brosur_jumlah'] ?> </td>
-								<td><?= $value['brosur_estimasi'] ?> Hari</td>
-								<td style="text-align: right"> Rp.<?= nominal($value['brosur_total']) ?></td>
-								<td><a class="fa fa-close table-shopping-remove"
-									   href="<?= base_url('hapus/brosur/' . $value['brosur_id']) ?>"
-									   onclick="return confirm('Hapus Pesanan? ')"></a></td>
-							</tr>
-						<?php
-						endforeach;
-						?>
+								<tr>
+									<td><img src="<?= base_url('assets/images/brosur/') . $value['brosur_foto'] ?>" alt="foto" style="width: 100%"></td>
+									<td><?= $value['brosur_bahan'] ?></td>
+									<td><?= $value['brosur_jumlah'] ?> </td>
+									<td><?= $value['brosur_estimasi'] ?> Hari</td>
+									<td style="text-align: right"> Rp.<?= nominal($value['brosur_total']) ?></td>
+									<td><a class="fa fa-close table-shopping-remove" href="<?= base_url('hapus/brosur/' . $value['brosur_id']) ?>" onclick="return confirm('Hapus Pesanan? ')"></a></td>
+								</tr>
+							<?php
+							endforeach;
+							?>
 						</tbody>
 					</table>
 					<div class="gap gap-small"></div>
@@ -192,8 +177,8 @@ elseif ($keranjang == !null):
 		</div>
 	</div>
 <?php
-else:
-	?>
+else :
+?>
 	<div class="container">
 		<div class="text-center"><i class="fa fa-cart-arrow-down empty-cart-icon"></i>
 			<p class="lead">Keranjang Kamu Kosong</p><a class="btn btn-primary btn-lg" href="<?= base_url() ?>">Pesan
